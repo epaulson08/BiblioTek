a94b10f7da0cd0e4eb3db9d188b32f403d1c7600
Eric Paulson <epaulson08@icloud.com>
2021-07-23 10:25:08 -0600

    implement back-end POST MyCollection controller

--- RefTracker/src/main/java/com/ericpaulsondev/reftracker/controllers/MyCollectionController.java
+++ RefTracker/src/main/java/com/ericpaulsondev/reftracker/controllers/MyCollectionController.java
@@ -9,9 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ericpaulsondev.reftracker.entities.JournalArticle;
 import com.ericpaulsondev.reftracker.entities.MyCollection;
 import com.ericpaulsondev.reftracker.entities.User;
 import com.ericpaulsondev.reftracker.services.MyCollectionService;
@@ -29,34 +30,44 @@ public class MyCollectionController {
 
 	@GetMapping("api/collections")
 	public List<MyCollection> findByUserUsername(Principal principal, HttpServletResponse resp) {
+		// admin: method not supported
 		if (isAdmin(principal)) {
 			resp.setStatus(405);
 			return null;
 		}
+		// user
 		else {
 			List<MyCollection> myColls = collServ.findByUserUsername(principal.getName());
+			// article belongs to user
 			if (myColls != null) {
 				resp.setStatus(200);
 				return myColls;
-			} else {
+			}
+			// article does not belong to user
+			else {
 				resp.setStatus(404);
 				return null;
-			}			
+			}
 		}
 	}
 
 	@GetMapping("api/all/collections/{id}")
 	public MyCollection findById(@PathVariable Integer id, HttpServletResponse resp, Principal principal) {
-		if (isAdmin(principal)) {		
+		// admin
+		if (isAdmin(principal)) {
 			MyCollection coll = collServ.findById(id);
+			// no such MyCollection
 			if (coll == null) {
 				resp.setStatus(404);
 				return null;
-			} else {
+			}
+			// MyCollection exists
+			else {
 				resp.setStatus(200);
 				return coll;
 			}
 		}
+		// user: access denied
 		else {
 			resp.setStatus(403);
 			return null;
@@ -66,19 +77,45 @@ public class MyCollectionController {
 	@GetMapping("api/all/collections/users/{userId}")
 	public List<MyCollection> findByUserId(@PathVariable Integer userId, HttpServletResponse resp,
 			Principal principal) {
+		// admin
 		if (isAdmin(principal)) {
 			List<MyCollection> toReturn = collServ.findByUserId(userId);
+			// requested user has MyCollections
 			if (toReturn != null) {
 				resp.setStatus(200);
 			}
+			// requested user has no MyCollections
 			else {
 				resp.setStatus(404);
 			}
 			return toReturn;
 		}
+		// user: access denied
 		else {
 			resp.setStatus(403);
 			return null;
+		}
+	}
+
+	@PostMapping("api/collections")
+	public MyCollection create(@RequestBody MyCollection myColl, HttpServletResponse resp, Principal principal) {
+		// admin: method not supported
+		if (isAdmin(principal)) {
+			resp.setStatus(405);
+			return null;
+		}
+		// user
+		else {
+			try {
+				Integer userId = userServ.showByUserName(principal.getName()).getId();
+				myColl = collServ.create(userId, myColl);
+				resp.setStatus(201);
+				return myColl;
+			} catch (NullPointerException npe) {
+				npe.printStackTrace();
+				resp.setStatus(400);
+				return null;
+			}
 		}
 	}
 
@@ -105,4 +142,5 @@ public class MyCollectionController {
 			return false;
 		}
 	}
+
 }
