5e1d776d4e78cd66e635acf5421444b507203346
Eric Paulson <epaulson08@icloud.com>
2021-09-17 06:38:37 -0600

    impl: can save an array of JournalArticles to docx

--- ngRefTracker/src/app/components/download-copy/download-copy.component.ts
+++ ngRefTracker/src/app/components/download-copy/download-copy.component.ts
@@ -17,6 +17,7 @@ import { ApaAuthorsPipe } from 'src/app/pipes/apa/apa-authors.pipe';
 
 export class DownloadCopyComponent implements OnInit {
   testArticle: JournalArticle;
+  testArr: JournalArticle[];
 
   constructor() {
   }
@@ -46,15 +47,43 @@ export class DownloadCopyComponent implements OnInit {
 
   }
 
-  createApaDocx(): Document {
+  createApaCitation(ja: JournalArticle): Paragraph {
     // Part A: non-italic: [authors (year). Title.]
     // Part B: italic: [journal, vol,]
     // Part C: non-italic: [pp.]
-    let authors: string = new ApaAuthorsPipe().transform(this.testArticle.authors);
-    let partA: string = `${authors} (${this.testArticle.yearPublished}). ${this.testArticle.title}. `;
-    let partB: string = `${this.testArticle.journal.name}, ${this.testArticle.volumeNum}, `;
-    let partC: string = `${this.testArticle.pages}.`;
+    let authors: string = new ApaAuthorsPipe().transform(ja.authors);
+    let partA: string = `${authors} (${ja.yearPublished}). ${ja.title}. `;
+    let partB: string = `${ja.journal.name}, ${ja.volumeNum}, `;
+    let partC: string = `${ja.pages}.`;
+    return new Paragraph({
+      style: "default",
+        children: [
+          new TextRun({
+            text: partA
+          }),
+          new TextRun({
+            text: partB,
+            italics: true,
+          }),
+          new TextRun({
+            text: partC,
+          })
+        ]})
+  }
 
+  createApaCitations(jas: JournalArticle[]): Paragraph[] {
+    let toReturn: Paragraph[] = [];
+    for (let i=0; i < jas.length; i++) {
+      toReturn.push(this.createApaCitation(jas[i]));
+      if (i !== jas.length - 1) {
+        toReturn.push(new Paragraph({style: "default", children: [new TextRun({text: ""})]}));
+      }
+    }
+    return toReturn;
+  }
+
+  createApaDocx(): Document {
+    let citations: Paragraph[] = this.createApaCitations(this.testArr);
     return new Document({
       styles: {
         paragraphStyles: [
@@ -73,22 +102,7 @@ export class DownloadCopyComponent implements OnInit {
       },
       sections: [
         {
-          children: [
-            new Paragraph({
-              style: "default",
-              children: [
-                new TextRun({
-                    text: partA
-                }),
-                new TextRun({
-                    text: partB,
-                    italics: true,
-                }),
-                new TextRun({
-                    text: partC,
-                })],
-            }),
-          ],
+          children: citations
         },
       ],
     });
@@ -202,5 +216,7 @@ export class DownloadCopyComponent implements OnInit {
       ];
       this.testArticle.pages = "7-12";
       this.testArticle.issueNum = null;
+
+      this.testArr = [this.testArticle, this.testArticle, this.testArticle];
     }
   }
