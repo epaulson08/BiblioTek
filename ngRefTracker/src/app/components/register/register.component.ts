f35775b8eb9f49eaa07bef696dc99846f46bab1d
Eric Paulson <epaulson08@icloud.com>
2021-10-09 23:07:29 -0600

    fix register page styling

--- ngRefTracker/src/app/components/register/register.component.ts
+++ ngRefTracker/src/app/components/register/register.component.ts
@@ -2,39 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   newUser: User = new User();
+  chosenPalette: string = '-A';
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   register(registerForm) {
-
     this.newUser.username = registerForm.uname.value;
     this.newUser.password = registerForm.pass.value;
 
     this.auth.register(this.newUser).subscribe(
-      data => {
+      (data) => {
         localStorage.setItem('username', this.newUser.username);
         this.auth.login(this.newUser.username, this.newUser.password).subscribe(
-          next => {
+          (next) => {
             this.router.navigateByUrl('/user-dashboard');
           },
-          error => {
+          (error) => {
             console.error('RegisterComponent.register(): error logging in.');
           }
         );
       },
-      err => {
+      (err) => {
         console.error('RegisterComponent.register(): error registering.');
         console.error(err);
       }
