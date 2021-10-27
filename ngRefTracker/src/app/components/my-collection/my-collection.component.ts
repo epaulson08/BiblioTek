830b38cf775a840be9e181e24b5756c72b029003
Eric Paulson <epaulson08@icloud.com>
2021-10-26 20:09:11 -0600

    update MyCollectionComponent to show Cite All UI

--- ngRefTracker/src/app/components/my-collection/my-collection.component.ts
+++ ngRefTracker/src/app/components/my-collection/my-collection.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CitationStyle } from 'src/app/models/citation-style';
 import { MyCollection } from 'src/app/models/my-collection.model';
 import { AuthService } from 'src/app/services/auth.service';
+import { CitationStyleService } from 'src/app/services/citation-style.service';
 import { MyCollectionService } from 'src/app/services/my-collection.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-my-collection',
   templateUrl: './my-collection.component.html',
-  styleUrls: ['./my-collection.component.css']
+  styleUrls: ['./my-collection.component.css'],
 })
 export class MyCollectionComponent implements OnInit {
-
   /////////
   // FIELDS
 
@@ -19,11 +20,13 @@ export class MyCollectionComponent implements OnInit {
   collId: number;
   coll: MyCollection;
   chosenPalette: string;
+  citationStyles: CitationStyle[];
 
   // UI
   listView: boolean = false;
   expandedView: boolean = true;
   editView: boolean = false;
+  showCiteAllUi: boolean = false;
 
   // CRUD
   editedMyCollection: MyCollection;
@@ -32,7 +35,13 @@ export class MyCollectionComponent implements OnInit {
   // METHODS
 
   // initialization
-  constructor(private auth: AuthService, private route: ActivatedRoute, private collServ: MyCollectionService, private userServ: UserService) { }
+  constructor(
+    private auth: AuthService,
+    private route: ActivatedRoute,
+    private collServ: MyCollectionService,
+    private userServ: UserService,
+    private csServ: CitationStyleService,
+  ) {}
 
   ngOnInit(): void {
     this.auth.guardRoute();
@@ -40,46 +49,72 @@ export class MyCollectionComponent implements OnInit {
     this.setExpandedView();
     this.collId = +this.route.snapshot.paramMap.get('collId');
     this.loadColl();
+    this.loadCitationStyles();
   }
 
   loadColl(): MyCollection {
     this.collServ.findByIdAsUser(this.collId).subscribe(
-      success => {
+      (success) => {
         this.coll = success;
         this.editedMyCollection = success;
         return success;
       },
-      failure => {
+      (failure) => {
         console.error(failure);
-      });
-      return null;
+      }
+    );
+    return null;
+  }
+
+  loadCitationStyles() {
+    this.csServ.findAll().subscribe(
+      (success) => {
+        this.citationStyles = success;
+        // alphabetize by abbreviation:
+        this.citationStyles.sort((a, b) =>
+          a.abbreviation.localeCompare(b.abbreviation)
+        );
+        return success;
+      },
+      (failure) => {
+        console.error(failure);
+      }
+    );
+    return null;
   }
 
   // UI
   setListView(): void {
     this.listView = true;
     this.expandedView = false;
+    this.showCiteAllUi = false;
   }
 
   setExpandedView(): void {
     this.expandedView = true;
     this.listView = false;
+    this.showCiteAllUi = false;
   }
 
   showEditView(): void {
     this.editView = true;
   }
 
+  citeAll(): void {
+    this.showCiteAllUi = true;
+    this.listView = false;
+    this.expandedView = false;
+  }
+
   // CRUD
   updateMyCollection(updatedVersion: MyCollection) {
     this.collServ.update(this.collId, updatedVersion).subscribe(
-      success => {
+      (success) => {
         this.coll = success;
       },
-      failure => {
+      (failure) => {
         console.error(failure);
       }
-    )
+    );
   }
-
 }
